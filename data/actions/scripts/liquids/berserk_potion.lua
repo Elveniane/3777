0208234b0f2a3433bc763947772d4de777251f1e
Elveniane <48421496+Elveniane@users.noreply.github.com>
2019-03-11 01:32:33 +0100

    Update berserk_potion.lua

--- data/actions/scripts/liquids/berserk_potion.lua
+++ data/actions/scripts/liquids/berserk_potion.lua
@@ -4,6 +4,11 @@ setConditionParam(condition, CONDITION_PARAM_SKILL_MELEE, 5)
 setConditionParam(condition, CONDITION_PARAM_SKILL_SHIELD, -10)
 
 function onUse(cid, item, fromPosition, itemEx, toPosition)
+	if(not isKnight(cid)) then
+		doPlayerSendCancel(cid, "Only knights may drink this fluid.")
+	return true
+	end
+	
 	if(doAddCondition(cid, condition)) then
 		doSendMagicEffect(fromPosition, CONST_ME_MAGIC_RED)
 		doRemoveItem(item.uid)
